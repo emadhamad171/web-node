0f51bb2e10010c30a4edb65be102a79aa20b80f5
dusik <dusikeh@gmail.com>
2022-10-16 13:39:17 +0300

    updating info for main screen, changing tsconfig to and package.json to be able to use .ts extension

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-console.log('one');
-let ww = process.env.NODE_ENV.trim.toLowerCase();
-if (ww === 'production') {
-  console.log('Another one');
-  console.log('              test');
-}
-console.log('one');
-if (ww === 'production') {
-  console.log('Another one');
-} else {
-  console.log('test');
-}
+// Read Environment Parameters
+import * as http from 'http';
+
+const port = Number(process.env.PORT) || 3001;
+const greeting = 'With best regards, Hamad Emad';
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end(`${greeting}\n`);
+});
+server.listen(port);
