dc144aff0b9c4ea3a706cbd7d3d35134c435a499
dusik <dusikeh@gmail.com>
2022-10-16 07:38:44 +0300

    adding dev branch to origin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 console.log('one');
-var ww = process.env.NODE_ENV.trim.toLowerCase();
+let ww = process.env.NODE_ENV.trim.toLowerCase();
+if (ww === 'production') {
+  console.log('Another one');
+  console.log('              test');
+}
+console.log('one');
 if (ww === 'production') {
   console.log('Another one');
 } else {
