d676c949645f80f766e2ede16d59d9ab822a0e28
dusik <dusikeh@gmail.com>
2022-10-18 22:01:21 +0300

    adding hosting and function deploy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // Read Environment Parameters
 import * as http from 'http';
 
+// The Firebase Admin SDK to access Firestore.
+
 const port = Number(process.env.PORT) || 3001;
 const greeting = 'With best regards, Hamad Emad';
 const server = http.createServer((request, response) => {
